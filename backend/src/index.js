020517b088e361b8a18d572074d7ea7937e82005
Rajeev <pmdbrajeev1@gmail.com>
2025-01-28 23:48:53 +0530

    Add dotenv file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import connectDB from "./lib/connectDB.js";
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(50001, () => {
+app.listen(PORT, () => {
+  connectDB();
   console.log("Server is listening");
 });
