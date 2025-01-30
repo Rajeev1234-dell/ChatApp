89df563c0865d7e2c065f6670a8288afc60a4f60
Rajeev <pmdbrajeev1@gmail.com>
2025-01-30 23:55:18 +0530

    Implement signup functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,48 @@
-export const signUp = (req, res) => {
-  res.send("signup route");
+import generateToken from "../lib/generateToken.js";
+import User from "../model/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signUp = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 6 characters" });
+  }
+
+  //   res.send("login route");
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error Message", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
