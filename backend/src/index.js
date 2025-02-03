827edf9bef88fefec2de77bc6f911ff3a97dbc1f
Rajeev <pmdbrajeev1@gmail.com>
2025-02-03 23:47:53 +0530

    Add message route

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
 import connectDB from "./lib/connectDB.js";
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoute);
 
 app.listen(PORT, () => {
   connectDB();
